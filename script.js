ea6a8025dedc7a076d104667f77d91507a1ac898
mongbean <zhimingqi9@gmail.com>
2025-02-09 18:27:26 -0800

    draggable

--- script.js
+++ script.js
@@ -1,24 +1,46 @@
 const draggables = document.querySelectorAll('.draggable');
 const dropZone = document.getElementById('drop-zone');
 
-draggables.forEach(draggable => {
-    draggable.addEventListener('dragstart', () => {
-        draggable.classList.add('dragging');
-    });
+function dragElement(element) {
+    var offsetX = 0, offsetY = 0, curX = 0, curY = 0;
+    element.onmousedown = dragMouseDown;
 
-    draggable.addEventListener('dragend', () => {
-        draggable.classList.remove('dragging');
-    });
-});
 
-dropZone.addEventListener('dragover', (e) => {
-    e.preventDefault();
-});
-
-dropZone.addEventListener('drop', (e) => {
-    e.preventDefault();
-    const draggedElement = document.querySelector('.dragging');
-    if (draggedElement) {
-        dropZone.appendChild(draggedElement);
+    function dragMouseDown(e) {
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        curX = e.clientX;
+        curY = e.clientY;
+        document.addEventListener("mouseup", closeDragElement);
+        // call a function whenever the cursor moves:
+        document.addEventListener("mousemove", elementDrag);
     }
-});
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        offsetX = e.clientX-curX;
+        offsetY = e.clientY-curY;
+        console.log("startX: " + curX + " startY: " + curY);
+        console.log("e.clientX: " + e.clientX + " e.clientY: " + e.clientY);
+        console.log("offsetX: " + offsetX + " offsetY: " + offsetY);
+        console.log("element.offsetTop: " + element.offsetTop + " element.offsetLeft: " + element.offsetLeft);
+        console.log("element.style.top: " + element.style.top + " element.style.left: " + element.style.left);
+        // set the element's new position:
+        element.style.top = (element.offsetTop + offsetY) + "px";
+        element.style.left = (element.offsetLeft + offsetX) +"px";
+
+        curX = e.clientX;
+        curY = e.clientY;
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.removeEventListener("mouseup", closeDragElement);
+        document.removeEventListener("mousemove", elementDrag);
+    }
+}
+
+draggables.forEach(draggable => {dragElement(draggable);});
+console.log('script.js loaded');
